1ce30d71044f64243be5c96939ba2fc885b7fb1c
Tyler Pannell <pannelltyler91@gmail.com>
2021-05-18 21:41:01 -0400

    just added fetch to signup

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,7 +1,22 @@
+import e from 'express';
 import {Component} from 'react';
 
 class Signup extends Component{
     render(){
+        _handleSubmit = (e) =>{
+            e.preventDefault();
+             fetch('http://localhost:3001/api/correctanswers', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({userSelection:userSelection}), //request body that will show in server.js//
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+        }
+            )}
         return(
             <div>
                 <form>
