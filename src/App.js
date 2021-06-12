d28501d6cd9c2b843c7829c7a0d12d04c5c47e22
unknown <you@example.com>
2021-06-12 10:30:55 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Navagation from './components/navagation';
 import {Component} from 'react';
+import {BrowserRouter as Router,Switch,Route,NavLink} from 'react-router-dom';
+import Login from '../src/components/login';
+import Signup from '../src/components/signup';
 import './App.css';
 
-
-
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>G4L</h1>
+      </header>
       <Router>
-      <h1>G4L</h1>
-    <Navagation/>
+        <NavLink to='/login'>Login</NavLink>
+        <NavLink to='/signup'>Signup</NavLink>
+        <Switch>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/signup' component={Signup}></Route>
+        </Switch>
       </Router>
-
     </div>
-    );
-  }
+  );
 }
+
 export default App;
